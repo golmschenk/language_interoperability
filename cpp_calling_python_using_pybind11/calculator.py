4e835ed6104839b6d2cfe848ccf2d69040038470
Greg Olmschenk <greg@olmschenk.com>
2021-10-02 13:08:50 -0400

    Correct CMake file and messages for C++ calling Python

--- cpp_calling_python_using_pybind11/calculator.py
+++ cpp_calling_python_using_pybind11/calculator.py
@@ -1,5 +1,5 @@
 def add(operand0, operand1):
-    print('Message from Python `add` function.')
+    print('Message from Python.')
     sum_ = operand0 + operand1
     print(f'Sum in Python is {sum_}.')
     return sum_
--- cpp_calling_python_using_pybind11/main.cpp
+++ cpp_calling_python_using_pybind11/main.cpp
@@ -3,9 +3,10 @@
 namespace py = pybind11;
 
 int main() {
+    std::cout << "Message from C++." << std::endl;
     py::scoped_interpreter guard{}; // Start the interpreter and keep it alive.
     py::module_ calculator = py::module_::import("calculator");
     py::object result = calculator.attr("add")(1, 2);
     int number = result.cast<int>();
-    std::cout << "Sum in C is " << number << "." << std::endl;
+    std::cout << "Sum in C++ is " << number << "." << std::endl;
 }
