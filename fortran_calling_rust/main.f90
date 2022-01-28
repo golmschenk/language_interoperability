79a74103de420d72b46406ff100feee9e6ec70b9
Greg Olmschenk <greg@olmschenk.com>
2022-01-28 18:10:59 -0500

    Rename the multiplication arguments to be more clear. Add `intent`s to the Fortran interface function

--- fortran_calling_rust/main.f90
+++ fortran_calling_rust/main.f90
@@ -2,10 +2,10 @@ program main
     use, intrinsic :: iso_c_binding, only : c_float
     implicit none
     interface
-        real(c_float) function multiply(a, b) bind(c, name = 'multiply')
+        real(c_float) function multiply(factor0, factor1) bind(c, name = 'multiply')
             import :: c_float
-            real(c_float), value :: a
-            real(c_float), value :: b
+            real(c_float), value, intent(in) :: factor0
+            real(c_float), value, intent(in) :: factor1
         end function
     end interface
     write(*, *) "Message from Fortran."
--- fortran_calling_rust/src/lib.rs
+++ fortran_calling_rust/src/lib.rs
@@ -1,7 +1,7 @@
 #[no_mangle]
-pub extern "C" fn multiply(a: f32, b: f32) -> f32 {
+pub extern "C" fn multiply(factor0: f32, factor1: f32) -> f32 {
     println!("Message from Rust.");
-    let product = a * b;
+    let product = factor0 * factor1;
     println!("Product in Rust is {product}");
     return product;
 }
